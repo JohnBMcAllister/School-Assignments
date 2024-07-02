8de98e2a0406091a15471ef2b67954f3025b2448
John Brennan McAllister <143151828+JohnBMcAllister@users.noreply.github.com>
2024-07-01 20:27:02 -0500

    Project 3 added to library

---
+++ COMP 2710/Project 3/project3_McAllister_UserID.cpp
@@ -0,0 +1,13 @@
+
+//Import Libraries
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+//Global Variables
+const int MAX_SIZE = 100;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
