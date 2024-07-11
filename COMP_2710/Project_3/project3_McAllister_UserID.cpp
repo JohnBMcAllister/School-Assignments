b12a3c78407d3fea40bc321392206c6d8c5a7117
John Brennan McAllister <143151828+JohnBMcAllister@users.noreply.github.com>
2024-07-11 15:25:43 -0500

    functional prototype

--- COMP_2710/Project_3/project3_McAllister_UserID.cpp
+++ COMP_2710/Project_3/project3_McAllister_UserID.cpp
@@ -51,6 +51,15 @@ vector<int> merge(vector<int>, vector<int>);
 void to_string(string, vector<int>);
 
 /*
+ * Sorts the 3rd vector after merging by using Gnome Sort
+ * Used gnome sort because it is simple and I thought it was funny.
+ *
+ * Time Complexity: O(N^2)
+ * Parem: Third vector containing values of the two merged vectors.
+ */
+void gnomeSort(vector<int>&);
+
+/*
  * Merge the numbers in two specified files and wriate all the numbers
  * to a specified third file sorted in ascending order.
  *
@@ -77,48 +86,58 @@ int main() {
 
   if(!check_file(file1)) {
    cout << "Error: File does not exist." << endl;
-   cin.clear(); // Clear the error state
-   cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignore any remaining input
+   /* Ran into a bug where if user inputted c++ code, the cin wouldn't
+    * clear and run through the do-while loop mutlipe times. Found a site
+    * that taught me how to clear cin */
+   cin.clear();
   }
  } while (cin.fail() || !check_file(file1));
 
  // Get and display numbers from file one.
  numbers1 = read_file(file1);
 
- cout << "The list of " << numbers1.size() << " in file " << file1 << "is:" << endl;
-
- //Clion suggested I used ranged-based loop instead of int i = 0; i < numbers1.size(); i++
- // For loop to print the numbers in the file
- for(int i : numbers1) {
-  cout << i << endl;
- }
+ to_string(file1, numbers1);
 
  /*
   * Get name of file two from User.
   * Validate the existence of the file two.
   */
  do {
-  cout << "Enter the second input file name: ";
+  cout << "\nEnter the second input file name: ";
   cin >> file2;
 
-  check_file(file2);
-
+  if(!check_file(file2)) {
+   cout << "Error: File does not exist." << endl;
+   cin.clear();
+   cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  }
  } while (cin.fail() || !check_file(file2));
 
  // Get and display numbers from file one.
  numbers2 = read_file(file2);
 
+ to_string(file2, numbers2);
+
  // Combine list and display the sorted result.
  numbers3 = merge(numbers1, numbers2);
 
+cout << "\nThe sorted list of " << numbers3.size() << " numbers is: ";
+ for(int numbers : numbers3) {
+  cout << numbers << " ";
+ }
 
  // Get name of output file.
  do {
+  cout << "\nEnter the output file name: ";
+  cin >> file3;
 
  } while (cin.fail());
 
  // Write combined vector to output file.
  write_file(file3, numbers3);
+
+ cout << "*** Please check the new file - " << file3 << " ***\n"
+  << "*** Goodbye. ***";
 
  return 0;
 }
@@ -153,8 +172,11 @@ vector<int> read_file(string file) {
 
  // Add each number in the file to a vector.
  stream.open(file.c_str());
- while (stream.good()) {
-  stream >> i;
+ /* Originally used while(stream.good()), but encounter a bug
+  * where it would push back the last int of the array twice
+  * ChatGPT suggested using (stream >> i) because it checks the stream
+  * state immediately after attempting to read a number. */
+ while (stream >> i) {
   v.push_back(i);
  }
 
@@ -163,11 +185,28 @@ vector<int> read_file(string file) {
 
 void write_file(string file, vector<int> v) {
  // Output file stream. (ofstream)
+ ofstream OutFile(file);
 
+ for(int numbers : v) {
+  OutFile << numbers << endl;
+ }
+
+ OutFile.close();
 }
 
 vector<int> merge(vector<int> v1, vector<int> v2) {
 vector<int> v3;
+
+ for(int number1: v1) {
+  v3.push_back(number1);
+ }
+
+ for(int number2: v2) {
+  v3.push_back(number2);
+ }
+
+ gnomeSort(v3);
+
  return v3;
 }
 
@@ -176,9 +215,28 @@ void to_string(string file, vector<int> v) {
  // Vector interator number. (unsigned short)
  unsigned short i;
 
+ cout << "The list of " << v.size() << " numbers in file " << file << " is:" << endl;
+
  // Display the numbers contained in a vector
+ for (i; i < v.size(); i++) {
+  cout << v[i] << endl;
+ }
 }
 
+void gnomeSort(vector<int>& v3) {
+ int index = 0;
 
+ while (index < v3.size()) {
+  if (index == 0) {
+   index++;
+  }
+  if (v3[index] >= v3[index - 1]) {
+   index++;
+  } else {
+   swap(v3[index], v3[index - 1]);
+   index--;
+  }
+ }
+ }
 
 
